d3e6e3f5b963c330281c46fafd865e5890e8c066
chrixus88 <chrisolisadebe@gmail.com>
2026-01-23 10:29:27 +0100

    updated Ec2 infrasture by creating a keypair for it

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,13 @@
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "terraform-key"
+  public_key = file("terraform-key.pub")
+}
+
+
 resource "aws_instance" "netflix" {
   ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t2.xlarge"
-
+  key_name = aws_key_pair.ec2_key.key_name
   tags = {
     Name = "HelloWorld"
   }
